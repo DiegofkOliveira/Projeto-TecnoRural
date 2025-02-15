5dc4acad0cb340946d0ecbc775c1064062effa06
Diego Oliveira <diegofkoliveira@gmail.com>
2025-02-15 13:10:43 -0300

    AgroTech New End
    New Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Rotas from './routes/routes.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Rotas />
+    <Analytics/>
   </StrictMode>,
 )
